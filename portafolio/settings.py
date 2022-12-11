4525e106f0857226de3421121405737d4e77931d
Joel Ramirez Garay <113269346+EnriqueR20@users.noreply.github.com>
2022-12-11 14:45:04 -0500

    Update settings.py

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -126,7 +126,7 @@ import os
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
-
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'aplicacion/static'),)
 
 
 # Default primary key field type
