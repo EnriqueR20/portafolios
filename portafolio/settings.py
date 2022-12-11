3b15b9e8bbde4014b5de789338dbc976e1f0088b
Joel Ramirez Garay <113269346+EnriqueR20@users.noreply.github.com>
2022-12-11 14:38:12 -0500

    static
    imagenes

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = 'static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'aplicacion/static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = 'media/'
 
 
