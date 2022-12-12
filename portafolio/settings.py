9bcfb16e2ab1bf59ef5215a674c00f9be61b39cd
Joel Ramirez Garay <113269346+EnriqueR20@users.noreply.github.com>
2022-12-11 19:22:14 -0500

    ima
    creadno

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -133,6 +133,7 @@ STATIC_ROOT= BASE_DIR / "staticfiles"
 
 STATICFIlES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
